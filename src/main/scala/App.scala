8f68db61b2c99566567fa6784c587c8f23c4f6d3
gaurdianAQ <gaurdianaq@protonmail.com>
2022-02-26 22:30:04 -0500

    Should bind to the correct port now

--- src/main/scala/App.scala
+++ src/main/scala/App.scala
@@ -4,6 +4,7 @@ import akka.http.scaladsl.Http
 import akka.http.scaladsl.model._
 import akka.http.scaladsl.server.Directives._
 import scala.io.StdIn
+import scala.sys.env
 
 @main def main =
     implicit val system = ActorSystem(Behaviors.empty, "my-system")
@@ -15,9 +16,9 @@ import scala.io.StdIn
             get
                 complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, "<h1>Say hello to akka-http</h1>"))
                 
-    val bindingFuture = Http().newServerAt("localhost", 8080).bind(route)
+    val bindingFuture = Http().newServerAt("127.0.0.1", sys.env("PORT").toInt).bind(route)
 
-    println(s"Server now online. Please navigate to http://localhost:8080/hello\nPress RETURN to stop...")
+    println(s"Server now online. Please navigate to http://127.0.0.1${sys.env("PORT")}/hello\nPress RETURN to stop...")
     StdIn.readLine() // let it run until user presses return
     bindingFuture
       .flatMap(_.unbind()) // trigger unbinding from the port
